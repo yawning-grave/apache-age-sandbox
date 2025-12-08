d4db4026b69a1b0eb64251568760b5a6d24591ea
yawning-grave <j_@mailbox.org>
2025-12-08 04:15:46 +0100

    Add files via upload

--- AgeSandbox.cs
+++ AgeSandbox.cs
@@ -8,7 +8,7 @@ using Npgsql.Age.Types;
 using static apache_age_console.AgeHelpers;
 
 const string connectionString = "Host=localhost;Port=5432;Username=user;Password=password;Database=postgres";
-const string graph = "test_graph";
+const string graph = "test_graph2";
 
 var datasource = AgeDataSourceBuilder(connectionString).Build();
 
@@ -34,41 +34,44 @@ async Task CreateAndReadNodes()
 async Task CreateParameterizedNodes()
 {
     await CreateGraphIfNotExists(datasource, graph);
-    await CypherNonQueryAsync(datasource, graph, "MATCH (n) DELETE n");
-    
-    await CypherNonQueryAsync(datasource, graph, 
-        "CREATE (p:Person) SET p.name = $name, p.age = $age", 
+    await CypherNonQueryAsync(datasource, graph, "MATCH (n) DETACH DELETE n");
+
+    await CypherNonQueryAsync(datasource, graph,
+        "CREATE (p:Person) SET p.name = $name, p.age = $age",
         @"{""name"": ""Alice"", ""age"": 24}");
 
     await CypherNonQueryAsync(datasource, graph,
         "CREATE (p:Person { age: $age, name: $name })",
         JsonSerializer.Serialize(new { name = "Bob", age = 23 }));
-    
+
     await CypherNonQueryAsync(datasource, graph,
         "CREATE (p:Person { age: $age, name: $name })",
         JsonSerializer.Serialize(new Dictionary<string, object> { ["name"] = "Candace", ["age"] = 25 }));
-    
+
     await using var connection = datasource.CreateConnection();
     await connection.OpenAsync();
-    
+
     var data = JsonSerializer.Serialize(
-        new { rows = new object[]
+        new
         {
-            new { abFriends = new { since = "2023" } },
-            new { bcColleagues = new { since = "2020", job = "software-dev" } },
-            new { bcHasCrushOn = new { since = "hello?" } }
-        }});
-    
+            rows = new object[]
+            {
+                new { abFriends = new { since = "2023" } },
+                new { bcColleagues = new { since = "2020", job = "software-dev" } },
+                new { bcHasCrushOn = new { since = "hello?" } }
+            }
+        });
+
     // Direct cypher query parametrization
     var cmd = new NpgsqlCommand(@$"
         SELECT * FROM cypher('{graph}', $$
             UNWIND $rows AS row
             CREATE (p:Person) SET p = row
             RETURN p
-        $$, @rows) AS (r agtype);", connection);
-    cmd.Parameters.Add(new NpgsqlParameter("rows", new Agtype(data)) { DataTypeName = "agtype" });
+        $$, @datarows) AS (r agtype);", connection);
+    cmd.Parameters.Add(new NpgsqlParameter("datarows", new Agtype(data)) { DataTypeName = "agtype" });
     var reader = await cmd.ExecuteNonQueryAsync();
-    
+
 
     var pstmt = new NpgsqlCommand($@"PREPARE ag_create_people(agtype) AS
             SELECT * FROM cypher('{graph}', $$ 
@@ -77,76 +80,79 @@ async Task CreateParameterizedNodes()
                 RETURN p
         $$, $1) AS (r agtype);
     ", connection);
-     
-        await pstmt.ExecuteNonQueryAsync();
 
- 
+    await pstmt.ExecuteNonQueryAsync();
 
-        // var exec = new NpgsqlCommand("EXECUTE ag_create_people(@data)", connection);
-        // var param = new NpgsqlParameter("data", new Agtype(data));
-        // // Specify the DataTypeName to inform Npgsql to serialize as agtype
-        // param.DataTypeName = "agtype";
-        // exec.Parameters.Add(param);
 
-        // var exec = new NpgsqlCommand($"EXECUTE ag_create_people('{data}')", connection);
-        
-        await exec.ExecuteNonQueryAsync();
+    // var exec = new NpgsqlCommand("EXECUTE ag_create_people(@data)", connection);
+    // var param = new NpgsqlParameter("data", new Agtype(data));
+    // // Specify the DataTypeName to inform Npgsql to serialize as agtype
+    // param.DataTypeName = "agtype";
+    // exec.Parameters.Add(param);
 
-    // await using var pstmt = new NpgsqlCommand(
-    //     @$"PREPARE ag_create_and_connect_people(agtype) AS
-    //         SELECT * FROM cypher('{graph}', $$ 
-    //             MATCH (a:Person {{ name: 'Alice' }}), 
-    //                 (b:Person {{ name: 'Bob' }}), 
-    //                 (c:Person {{ name: 'Candace'}}) 
-    //             UNWIND $rows AS row
-    //             WITH row.abFriends AS map_abFriends,
-    //                 row.bcColleagues AS map_bcColleagues,
-    //                 row.bcHasCrushOn AS map_bcHasCrushOn
-    //             CREATE (a)-[abFriends:FRIENDS]->(b),
-    //                 (b)-[baFriends:FRIENDS]->(a), 
-    //                 (b)-[bcColleagues:COLLEAGUES]->(c),
-    //                 (c)-[cbColleagues:COLLEAGUES]->(b),
-    //                 (b)-[bcHasCrushOn:HAS_CRUSH_ON]->(c)
-    //             SET abFriends = map_abFriends,
-    //                 baFriends = map_abFriends,
-    //                 bcColleagues = map_bcColleagues,
-    //                 cbColleagues = map_bcColleagues,                    
-    //                 bcHasCrushOn = map_bcHasCrushOn
-    //             RETURN a, b, c, abFriends, bcColleagues, bcHasCrushOn
-    //     $$, $1) AS (a agtype, b agtype, c agtype, abFriends agtype, bcColleagues agtype, bcHasCrushOn agtype)", connection);
-    // await pstmt.ExecuteNonQueryAsync();
+    // var exec = new NpgsqlCommand($"EXECUTE ag_create_people('{data}')", connection);
     //
-    // var data = JsonSerializer.Serialize(
-    //     new { rows = new object[]
-    //     {
-    //         new { abFriends = new { since = "2023" } },
-    //         new { bcColleagues = new { since = "2020", job = "software-dev" } },
-    //         new { bcHasCrushOn = new { since = "forever" } }
-    //     }});
-    //
-    // await using var exec = new NpgsqlCommand("EXECUTE ag_create_and_connect_people($1);", connection) { Parameters = { new NpgsqlParameter { DataTypeName = "agtype", Value = new Agtype(data) }}}; 
-    //
-    // Vertex a = default, b = default, c = default;
-    // Edge abFriends = default, bcColleagues = default, bcHasCrushOn = default;
-    //
-    // var execReader = await exec.ExecuteReaderAsync();
-    //
-    // while (await execReader.ReadAsync())
-    // {
-    //     a = ((Agtype)execReader["a"]).GetVertex();
-    //     b = ((Agtype)execReader["b"]).GetVertex();
-    //     c = ((Agtype)execReader["c"]).GetVertex();
-    //     abFriends = ((Agtype)execReader["abFriends"]).GetEdge();
-    //     bcColleagues = ((Agtype)execReader["bcColleagues"]).GetEdge();
-    //     bcHasCrushOn = ((Agtype)execReader["bcHasCrushOn"]).GetEdge();
-    //
-    //     break;
-    // }
+    // await exec.ExecuteNonQueryAsync();
 
-    // Console.WriteLine(a);
-    // Console.WriteLine(b);
-    // Console.WriteLine(c);
-    // Console.WriteLine(abFriends);
-    // Console.WriteLine(bcColleagues);
-    // Console.WriteLine(bcHasCrushOn);
-} 
+    await using var pstmt2 = new NpgsqlCommand(
+        @$"SELECT * FROM cypher('{graph}', $$ 
+                MATCH (a:Person {{ name: 'Alice' }}), 
+                    (b:Person {{ name: 'Bob' }}), 
+                    (c:Person {{ name: 'Candace'}}) 
+                UNWIND $rows AS row
+                WITH row.abFriends AS map_abFriends,
+                    row.bcColleagues AS map_bcColleagues,
+                    row.bcHasCrushOn AS map_bcHasCrushOn
+                CREATE (a)-[abFriends:FRIENDS]->(b),
+                    (b)-[baFriends:FRIENDS]->(a), 
+                    (b)-[bcColleagues:COLLEAGUES]->(c),
+                    (c)-[cbColleagues:COLLEAGUES]->(b),
+                    (b)-[bcHasCrushOn:HAS_CRUSH_ON]->(c)
+                SET abFriends = map_abFriends,
+                    baFriends = map_abFriends,
+                    bcColleagues = map_bcColleagues,
+                    cbColleagues = map_bcColleagues,                    
+                    bcHasCrushOn = map_bcHasCrushOn
+                RETURN a, b, c, abFriends, bcColleagues, bcHasCrushOn
+        $$, @datarowsii) AS (a agtype, b agtype, c agtype, abFriends agtype, bcColleagues agtype, bcHasCrushOn agtype);",
+        connection);
+
+    var data2 = JsonSerializer.Serialize(
+        new
+        {
+            rows = new object[]
+            {
+                new
+                {
+                    abFriends = new { since = "2023" },
+                    bcColleagues = new { since = "2020" },
+                    bcHasCrushOn = new { since = "forever" }
+                }
+            }
+        });
+
+    pstmt2.Parameters.Add(new NpgsqlParameter("datarowsii", new Agtype(data2)) { DataTypeName = "agtype" });
+    Vertex a = default, b = default, c = default;
+    Edge abFriends = default, bcColleagues = default, bcHasCrushOn = default;
+
+    var execReader = await pstmt2.ExecuteReaderAsync();
+
+    /*while (await execReader.ReadAsync())
+    {
+        a = ((Agtype)execReader["a"]).GetVertex();
+        b = ((Agtype)execReader["b"]).GetVertex();
+        c = ((Agtype)execReader["c"]).GetVertex();
+        abFriends = ((Agtype)execReader["abFriends"]).GetEdge();
+        bcColleagues = ((Agtype)execReader["bcColleagues"]).GetEdge();
+        bcHasCrushOn = ((Agtype)execReader["bcHasCrushOn"]).GetEdge();
+
+        break;
+    }
+
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+    Console.WriteLine(c);
+    Console.WriteLine(abFriends);
+    Console.WriteLine(bcColleagues);
+    Console.WriteLine(bcHasCrushOn);*/
+}
